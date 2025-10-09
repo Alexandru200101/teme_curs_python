832ee2d635d28d67d697bc6df5b62b1b1a9c43bc
Alexandru200101 <cataramba.alexandru@yahoo.com>
2025-10-09 10:59:30 +0300

    update

--- fast_api_baza_date/validari/criptare.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
-
-def hash_parola(parola: str) -> str:
-    return pwd_context.hash(parola)
-
-def verifica_parola(parola: str, hash_parola_db: str) -> bool:
-    return pwd_context.verify(parola, hash_parola_db)
